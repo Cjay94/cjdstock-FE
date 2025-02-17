8ce36b6d2e6a8a5efc3368646999714e8addba93
Cjay94 <cjephthah94@gmail.com>
2025-02-17 16:44:09 +0200

    Created Popular Product Card as well as updated api to link FE with DB

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,18 @@
+"use client"
+
+import CardPopularProducts from "./CardPopularProducts"
+
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+            <CardPopularProducts />
+            <div className="bg-orange-500 text-white row-span-3 xl:row-span-6">Component 2</div>
+            <div className="bg-yellow-500 text-white row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 ">Component 3</div>
+            <div className="bg-green-500 text-white row-span-3">Component 4</div>
+            <div className="bg-blue-500 text-white md:row-span-1 xl:row-span-2">Component 5</div>
+            <div className="bg-purple-500 text-white md:row-span-1 xl:row-span-2">Component 6</div>
+            <div className="bg-pink-500 text-white md:row-span-1 xl:row-span-2">Component 7</div>
+        </div>
     )
 }
 
