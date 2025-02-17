ce22828ed5ebd1c8875da7c1cd17a37e36434201
Cjay94 <cjephthah94@gmail.com>
2025-02-17 18:55:27 +0200

    completed Purchase Summary

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import CardPopularProducts from "./CardPopularProducts"
+import CardPurchaseSummary from "./CardPurchaseSummary"
 import CardSalesSummary from "./CardSalesSummary"
 
 const Dashboard = () => {
@@ -8,7 +9,7 @@ const Dashboard = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
             <CardPopularProducts />
             <CardSalesSummary />
-            <div className="bg-yellow-500 text-white row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 ">Component 3</div>
+            <CardPurchaseSummary />
             <div className="bg-green-500 text-white row-span-3">Component 4</div>
             <div className="bg-blue-500 text-white md:row-span-1 xl:row-span-2">Component 5</div>
             <div className="bg-purple-500 text-white md:row-span-1 xl:row-span-2">Component 6</div>
